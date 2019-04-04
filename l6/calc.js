8ac2d2267c76f1c2e0e12806bb1d95a6279642b6
kuro-kky <kurokky2020@gmail.com>
2019-04-04 11:33:58 +0200

    lab6 js calc

---
+++ l6/calc.js
@@ -0,0 +1,42 @@
+const fields = [
+    { txt: 1, row: 4, col:1 },
+    { txt: 2, row: 4, col:2 },
+    { txt: 3, row: 4, col:3 },
+    { txt: 4, row: 3, col:1 },
+    { txt: 5, row: 3, col:2 },
+    { txt: 6, row: 3, col:3 },
+    { txt: 7, row: 2, col:1 },
+    { txt: 8, row: 2, col:2 },
+    { txt: 9, row: 2, col:3 },
+    { txt: 0, row: 5, col:'1/3' },
+    { txt: '.', row: 5, col:3 },
+    { txt: '-', row: 5, col:4 },
+    { txt: '+', row: 4, col:4 },
+    { txt: '=', row: 3, col:4 },
+    { txt: 'C', row: 2, col:4 },
+    { txt: 'DISPLAY', row: 1, col:'1/5' },
+    /*
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.', '-', '+', '=', 'C', 'DISPLAY'
+    */
+];
+
+const init = () => {
+    const container = document.createElement('div');
+    container.id = 'container';
+    fields.forEach(el => {
+        const div = document.createElement('div');
+        div.textContent = el.txt;
+        div.style.gridColumn = el.col;
+        div.style.gridRow = el.row;
+        container.appendChild(div);
+//        console.log(div);
+    });
+    document.body.appendChild(container);
+/*
+    for(let i=0; i<fields.length; i++) {
+        console.log(fields[i]);
+    }
+*/
+}
+
+window.addEventListener('DOMContentLoaded', init);
